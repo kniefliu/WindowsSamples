e173952ed652e27055b7cdcce6662d6d547a10cb
WhitebrowSwordsman <kniefliu@gmail.com>
2020-03-26 23:31:59 +0800

    skia chrome/m79 original code.

---
+++ third_party/skia_m79/README.md
@@ -0,0 +1,4 @@
+# skia
+skia chrome/m76
+
+* auto compile, refer to [ReadMe](bin/README.md)
---
+++ third_party/skia_m79/bin/SetToolPath.bat
@@ -0,0 +1,10 @@
+@ECHO OFF
+
+SET SKIA_M63_BIN_PATH=%~dp0
+SET PATH=%SKIA_M63_BIN_PATH%;C:\Python27;%PATH%
+
+%comspec% /k ""C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvarsall.bat"" x64
+
+CD ../
+
+CMD /K
