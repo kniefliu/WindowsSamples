60f8f5018111003649aa9dc231e7f79c6635b217
kniefliu <kniefliu@163.com>
2020-08-09 15:40:20 +0800

    skia_m84 : new 'HelloD3D11Texture' demo.

---
+++ third_party/skia_m84/demo/HelloD3D11Texture.cpp
@@ -0,0 +1,134 @@
+/*
+* Copyright 2017 Google Inc.
+*
+* Use of this source code is governed by a BSD-style license that can be
+* found in the LICENSE file.
+*/
+
+#include "demo/HelloD3D11Texture.h"
+
+#include "include/core/SkCanvas.h"
+#include "include/core/SkFont.h"
+#include "include/core/SkGraphics.h"
+#include "include/core/SkSurface.h"
+#include "include/effects/SkGradientShader.h"
+
+
+using namespace sk_app;
+
+
+Application* Application::Create(int argc, char** argv, void* platformData) {
+    return new HelloD3D11Texture(argc, argv, platformData);
+}
+
+HelloD3D11Texture::HelloD3D11Texture(int argc, char** argv, void* platformData)
+        : fBackendType(Window::kNativeGL_BackendType)
+        , fRotationAngle(0) {
+#ifdef SK_BUILD_FOR_WIN
+    fBackendType = Window::kANGLE_BackendType;
+#endif
+    SkGraphics::Init();
+
+    fWindow = Window::CreateNativeWindow(platformData);
+    fWindow->setRequestedDisplayParams(DisplayParams());
+
+    // register callbacks
+    fWindow->pushLayer(this);
+
+    fWindow->attach(fBackendType);
+}
+
+HelloD3D11Texture::~HelloD3D11Texture() {
+    fWindow->detach();
+    delete fWindow;
+}
+
+void HelloD3D11Texture::updateTitle() {
+    if (!fWindow/* || fWindow->sampleCount() <= 1*/) {
+        return;
+    }
+
+    SkString title("Hello World ");
+#ifdef SK_BUILD_FOR_WIN
+    title.append(Window::kRaster_BackendType == fBackendType ? "Raster" : "ANGLE");
+#else
+    title.append(Window::kRaster_BackendType == fBackendType ? "Raster" : "OpenGL");
+#endif
+    fWindow->setTitle(title.c_str());
+}
+
+void HelloD3D11Texture::onBackendCreated() {
+    this->updateTitle();
+    fWindow->show();
+    fWindow->inval();
+}
+
+void HelloD3D11Texture::onPaint(SkSurface* surface) {
+    auto canvas = surface->getCanvas();
+
+    // Clear background
+    canvas->clear(SK_ColorWHITE);
+
+    SkPaint paint;
+    paint.setColor(SK_ColorRED);
+
+    // Draw a rectangle with red paint
+    SkRect rect = SkRect::MakeXYWH(10, 10, 128, 128);
+    canvas->drawRect(rect, paint);
+
+    // Set up a linear gradient and draw a circle
+    {
+        SkPoint linearPoints[] = { { 0, 0 }, { 300, 300 } };
+        SkColor linearColors[] = { SK_ColorGREEN, SK_ColorBLACK };
+        paint.setShader(SkGradientShader::MakeLinear(linearPoints, linearColors, nullptr, 2,
+                                                     SkTileMode::kMirror));
+        paint.setAntiAlias(true);
+
+        canvas->drawCircle(200, 200, 64, paint);
+
+        // Detach shader
+        paint.setShader(nullptr);
+    }
+
+    // Draw a message with a nice black paint
+    SkFont font;
+    font.setSubpixel(true);
+    font.setSize(20);
+    paint.setColor(SK_ColorBLACK);
+
+    canvas->save();
+    static const char message[] = "Hello D3D11Texture";
+
+    // Translate and rotate
+    canvas->translate(300, 300);
+    fRotationAngle += 0.2f;
+    if (fRotationAngle > 360) {
+        fRotationAngle -= 360;
+    }
+    canvas->rotate(fRotationAngle);
+
+    // Draw the text
+    canvas->drawSimpleText(message, strlen(message), SkTextEncoding::kUTF8, 0, 0, font, paint);
+
+    canvas->restore();
+}
+
+void HelloD3D11Texture::onIdle() {
+    // Just re-paint continously
+    fWindow->inval();
+}
+
+bool HelloD3D11Texture::onChar(SkUnichar c, skui::ModifierKey modifiers) {
+    if (' ' == c) {
+#ifdef SK_BUILD_FOR_WIN
+        fBackendType = Window::kRaster_BackendType == fBackendType ? Window::kANGLE_BackendType
+                                                                   : Window::kRaster_BackendType;
+#else
+        fBackendType = Window::kRaster_BackendType == fBackendType ? Window::kNativeGL_BackendType
+                                                                   : Window::kRaster_BackendType;
+#endif
+        fWindow->detach();
+        fWindow->attach(fBackendType);
+    }
+    return true;
+}
